b97b736ac1f19dc090c45bf429771de0ec66a6cc
NishatFariza <nishatfarzia78@gmail.com>
2022-05-18 18:29:33 +0600

    added middleware

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
   res.send('To Do List is running')
 })
